888ee0b10d936395e45194d6b48504d96b401a98
Yuliia Holub <140198j@gmail.com>
2022-01-27 20:16:49 +0200

    NodeList, Html collection

--- index.js
+++ index.js
@@ -10,10 +10,11 @@ function yellowButton(){
 // document.getElementsByTagName()
 //document.getElementsByClassName()
 
-// querySelector
-// querySelectorAll
+// querySelector - принимает любой валидный css селектор.Возвращает один элемент(первый встреченный)
+//Селекторы прописываем по правилам css
+// querySelectorAll - возвращает все элементы по валидному селектору
 
-const [firstButton] = document.getElementsByTagName('button')
+const [firstButton] = document.getElementsByTagName('button') //возвращает HTML-collection (живая коллекция)
 firstButton.addEventListener('click', allertMessage)
 
 const yellow_Button = document.getElementsByClassName('yellow-button'); 
@@ -24,3 +25,9 @@ for(const item of yellowButton){
 
 //вывести счетчик сколько нажимали
 
+// const p = document.querySelector('article > p') //возвращает NodeList - ведет себя как массив
+const ps = document.querySelectorAll('article > p') 
+
+const h1 = document.querySelector('h1')
+const img = document.querySelector('img')
+const span = document.querySelector('span')
